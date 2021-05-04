061a6277060e9edb1db8565aac7c64e1090ceb16
fabiloco <faalsaru@gmail.com>
2021-05-04 02:46:40 -0500

    petitions almost done

--- js/index.js
+++ js/index.js
@@ -3,24 +3,45 @@ let allPokemonsInfo = [];
 
 
 
-const getRandomNum = () => Math.round(Math.random() * 151 + 1);
+const getRandomNum = (max=151) => Math.round(Math.random() * max + 1);
 
 // Petición realizada con promises
 const getAllPokemons = () => {
     fetch(URL_ALL_POKEMONS)
         .then(res => res.json())
         .then(allPokemons => {
-            allPokemonsInfo = allPokemons.results;
-            console.log(allPokemonsInfo);
-            console.log(getRandomNum());
+            allPokemonsInfo = [...allPokemons.results];
+            getAnswers();
         });
 }
-
 // Petición realizada con async-await y fetch
 // const getAllPokemons = async () => {
 //     const request = await fetch(URL_ALL_POKEMONS);
 //     const allPokemons = await request.json();
 // }
+
+const getAnswers = (answers = 3) => {
+    const options = [];
+    let randomNum = getRandomNum();
+    let currentPokemon = allPokemonsInfo[randomNum];
+    options.push(currentPokemon);
+    while(options.length < answers) {
+        randomNum = getRandomNum();
+        currentPokemon = allPokemonsInfo[randomNum];
+        if(options.find(({ name }) => currentPokemon.name !== name)) {
+            options.push(currentPokemon);
+        }     
+    }
+    getSprite(options[0].url.substring(34).replace('/',''));
+}
+
+const getSprite = (id) => { 
+    fetch(`https://pokeapi.co/api/v2/pokemon-form/${id}`)
+        .then(res => res.json())
+        .then(pokeInfo => {
+            console.log(pokeInfo);
+        });    
+}
 
 getAllPokemons();
 
